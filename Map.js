19430f29f6ed62363e5b775bbf0c727769bbc64a
Faraz Khan <farazcsk@gmail.com>
2016-12-04 17:26:08 +0000

    Adds current location

--- Map.js
+++ Map.js
@@ -8,8 +8,8 @@ class Map extends React.Component {
       <Components.MapView
         style={{ flex: 1 }}
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
+          latitude: this.props.lat || 37.78825,
+          longitude: this.props.lng || -122.4324,
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
--- main.js
+++ main.js
@@ -2,17 +2,50 @@ import Exponent from 'exponent';
 import React from 'react';
 import {
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
 
 import Map from './Map';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: 0,
+      lng: 0,
+    };
+    this.getLocationAsync = this.getLocationAsync.bind(this);
+  }
+
+  componentWillMount() {
+    this.getLocationAsync();
+  }
+
+  async getLocationAsync() {
+    const { Location, Permissions } = Exponent;
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    if (status === 'granted') {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.setState({
+          ...this.state,
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+      });
+    } else {
+      throw new Error('Location permission not granted');
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Map />
+        <Map 
+          lat={this.state.lat}
+          lng={this.state.lng}
+        />
       </View>
     );
   }
