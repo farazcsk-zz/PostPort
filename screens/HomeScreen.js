5d607c3b4f9e0df99b911f8a6ae7bafc8fc296ef
Faraz Khan <farazcsk@gmail.com>
2017-01-01 12:01:30 +0000

    Adds HomeScreen component.

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+
+class HomeScreen extends React.Component {
+
+
+  render() {
+    return (
+      <Text> Honey I am home </Text>
+    );
+  }
+}
+
+export default HomeScreen;
