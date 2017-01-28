5e7d887fdf86638f4afe98ab74fde269b34626eb
Faraz Khan <farazcsk@gmail.com>
2017-01-28 17:00:56 +0000

    Removes unused apollo code from home.js

--- screens/Home.js
+++ screens/Home.js
@@ -1,19 +1,7 @@
 import React from 'react';
 import * as Animatable from 'react-native-animatable';
-import gql from 'graphql-tag';
-import { graphql } from 'react-apollo';
-import {
-  Card,
-  CardTitle,
-  CardAction,
-} from 'react-native-card-view';
 import {
   ScrollView,
-  StyleSheet,
-  Button,
-  Text,
-  TextInput,
-  ActivityIndicator,
   AsyncStorage,
   View,
   WebView,
@@ -63,62 +51,8 @@ class Home extends React.Component {
     );
   }
 }
-const loginMutation = gql`
-  mutation signinUser($email: String!, $password: String!) {
-    signinUser(email: { email: $email, password: $password }){
-      token
-      user {
-        id,
-        firstName,
-        lastName,
-        email,
-      }
-    }
-  }
-`;
 
-const card = {
-  card: {
-    marginTop: 100,
-    margin: 20,
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fafafa',
-    borderWidth: 2,
-    borderColor: '#843131',
-  },
-};
-
-const styles = StyleSheet.create({
-  input: {
-    fontFamily: 'roboto-bold',
-    width: 250,
-    height: 40,
-    margin: 20,
-  },
-  container: {
-    paddingTop: 100,
-    backgroundColor: '#fafafa',
-  },
-
-  welcome: {
-    fontFamily: 'roboto-mono-regular',
-    fontSize: 20,
-    textAlign: 'center',
-    color: '#3B3738',
-    margin: 10,
-  },
-
-  instructions: {
-    fontFamily: 'roboto-italic',
-    textAlign: 'center',
-    color: '#3B3738',
-    marginBottom: 5,
-  },
-});
-
-export default graphql(loginMutation)(Home);
+export default Home;
 
 
 
