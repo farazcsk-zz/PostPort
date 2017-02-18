3f76567683e76bbb25d5b251f83c92664a7bb1ef
Faraz Khan <farazcsk@gmail.com>
2017-02-18 06:33:18 -0800

    Adds login with facebook button to Home.

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,9 @@
 import React, { PropTypes } from 'react';
 import {
-  ScrollView,
+  View,
   Alert,
+  Button,
+  StyleSheet,
 } from 'react-native';
 import Exponent, { Components } from 'exponent';
 
@@ -19,16 +21,16 @@ class Home extends React.Component {
     },
   }
 
-  componentWillMount() {
+  login = () => {
     Exponent.Facebook.logInWithReadPermissionsAsync(
       '395836734110712', {
-      permissions: ['public_profile'],
-    })
+        permissions: ['public_profile'],
+      })
     .then((response) => {
       if (response.type === 'success') {
         // Get the user's name using Facebook's Graph API
         fetch(
-          `https://graph.facebook.com/me?access_token=${response.token}`
+          `https://graph.facebook.com/me?access_token=${response.token}`,
         )
         .then((response) => response.json())
         .then((responseData) => {
@@ -36,7 +38,7 @@ class Home extends React.Component {
             'Logged in!',
             `Hi ${responseData.name}!`,
           );
-        })
+        });
         // https:graph.facebook.com/me?fields=posts{place}&access_token=
       }
     });
@@ -44,15 +46,25 @@ class Home extends React.Component {
 
   render() {
     return (
-      <ScrollView>
-      </ScrollView>
+      <View style={styles.container}>
+        <Button
+          title="login with facebook"
+          onPress={() => console.log('hello')}
+        />
+      </View>
     );
   }
 }
 
 Home.propTypes = propTypes;
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
+
 export default Home;
-
-
 
