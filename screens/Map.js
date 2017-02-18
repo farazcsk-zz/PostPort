54b0ea885ffe0229b1473e834ceda1769fe20c86
Faraz Khan <farazcsk@gmail.com>
2017-02-18 13:14:34 -0800

    Cleans up map.

--- screens/Map.js
+++ screens/Map.js
@@ -1,16 +1,8 @@
 import Exponent, { Components } from 'exponent';
 import React, { Component, PropTypes } from 'react';
-import {
-  ActivityIndicator,
-  View,
-} from 'react-native';
-
-import Post from '../components/Post';
+import { View } from 'react-native';
 
 const propTypes = {
-  posts: PropTypes.any,
-  isLoading: PropTypes.bool.isRequired,
-  getPosts: PropTypes.func.isRequired,
 };
 
 class Map extends Component {
@@ -21,21 +13,17 @@ class Map extends Component {
     },
   }
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      region: {
-        latitude: 0,
-        longitude: 0,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      },
-      currentPost: null,
-    };
-  }
+  state = {
+    region: {
+      latitude: 0,
+      longitude: 0,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    },
+    currentPost: null,
+  };
 
   componentWillMount() {
-    this.props.getPosts();
     const { Location, Permissions } = Exponent;
     Permissions.askAsync(Permissions.LOCATION)
     .then((response) => {
@@ -60,51 +48,16 @@ class Map extends Component {
     });
   }
 
-  setPost = (post) => {
-    console.log('pin press');
-    this.setState({
-      ...this.state,
-      region: {
-        ...this.state.region,
-        latitude: post.location.latitude,
-        longitude: post.location.longitude,
-      },
-      currentPost: post,
-    });
-  }
-
   render() {
-    if (this.props.isLoading) {
-      return (
-        <ActivityIndicator
-          size="large"
-          color="#262626"
-        />
-      );
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Components.MapView
-            style={{ flex: 1 }}
-            region={this.state.region}
-          >
-            {this.props.posts.map(post => (
-              <Components.MapView.Marker
-                key={post.id}
-                coordinate={{
-                  latitude: post.location.latitude,
-                  longitude: post.location.longitude,
-                }}
-                title={post.caption ? post.caption.text : ''}
-                description={post.location ? post.location.name : ''}
-                onPress={() => this.setPost(post)}
-              />
-            ))}
-          </Components.MapView>
-          <Post post={this.state.currentPost} />
-        </View>
-      );
-    }
+    return (
+      <View style={{ flex: 1 }}>
+        <Components.MapView
+          style={{ flex: 1 }}
+          region={this.state.region}
+        >
+        </Components.MapView>
+      </View>
+    );
   }
 }
 
