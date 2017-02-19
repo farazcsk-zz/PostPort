f1c0692b4f012e3fbdeb0be1d287411e91fbb1d0
Faraz Khan <farazcsk@gmail.com>
2017-02-19 06:25:15 -0800

    Adds isLoading, success and error to home, success now navigates user to map screen.

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,9 +1,13 @@
 import { connect } from 'react-redux';
 
 import { getPosts } from '../posts/postActions';
+import { getIsLoading, getSuccess, getError } from '../posts/postsReducer';
 import Home from './Home';
 
 const mapStateToProps = (state) => ({
+  isLoading: getIsLoading(state.posts),
+  success: getSuccess(state.posts),
+  error: getError(state.posts),
 });
 
 export default connect(mapStateToProps, {
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,12 @@
 import { connect } from 'react-redux';
 
+import { getAllPosts } from '../posts/postsReducer';
 import Map from './Map';
 
 const mapStateToProps = (state) => {
-  return {};
+  return {
+    posts: getAllPosts(state.posts),
+  };
 };
 
 export default connect(mapStateToProps, {
